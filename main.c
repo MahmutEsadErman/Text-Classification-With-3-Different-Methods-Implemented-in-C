ea3229d0135924a030aa1a094cb4e045dcf70e98
Mahmut Esad <ermanesad56@gmail.com>
2023-12-07 13:58:03 +0300

    Saving file after each epoch feature added.

--- main.c
+++ main.c
@@ -3,16 +3,17 @@
 #include <time.h>
 #include "functionsForDictionaryAndHotVectors.h"
 
-#define OPFUNC 2
+enum Method {gd,sgd,adam};
+
+#define OPFUNC gd
 #define STARTW 0
 
 void setValuesToArray(double* arr, double value, int length);
-double gradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors);
-double stochasticGradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors,int stoch_size);
-double ADAM(int maxIter, int exampleNumber, double learning_rate, int dictlength,double* weights,bool ** hotvectors,double b1 ,double b2 ,int stoch_size);
+void gradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors, bool** testHotVectors, int numberOfTestSamples, FILE* outputFile);
+void stochasticGradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors,int stoch_size, bool** testHotVectors, int numberOfTestSamples, FILE* outputFile);
+void ADAM(int maxIter, int exampleNumber, double learning_rate, int dictlength,double* weights,bool ** hotvectors,double b1 ,double b2 ,int stoch_size, bool** testHotVectors, int numberOfTestSamples, FILE* outputFile);
 
 double test(bool** hotVectors, double* weights, int dictLength, int exampleNumber);
-void saveStatistics(char* path, double loss, double succesRate, double duration);
 
 double function(double wx){
     return tanh(wx);
@@ -37,21 +38,26 @@ double dotProduct(const double* w, const bool* x, int n){
 }
 
 int main() {
-    int i, numberOfTrainingExamples, numberOfTestExamples;
+    int i, numberOfTrainingSamples, numberOfTestSamples;
     dict dictionary;
     char* group1_training_filePath = "dataset/businessTraining.txt";
     char* group2_training_filePath = "dataset/sportTraining.txt";
     char* group1_test_filePath = "dataset/businessTest.txt";
     char* group2_test_filePath = "dataset/sportTest.txt";
     char* outputFilePath = "dataset/statistics.txt";
+
     bool** trainingHotVectors;
     bool** testHotVectors;
     double* weights;
-    clock_t start_time, end_time;
+    clock_t start_time;
+
     FILE* group1TrainingFile;
     FILE* group2TrainingFile;
     FILE* group1TestFile;
     FILE* group2TestFile;
+    FILE* outputFile;
+
+    srand(time(NULL)); // This is for rand() work properly
 
     // Loading Dictionary
     loadDictionary("dataset/dictionary.txt", &dictionary);
@@ -62,16 +68,17 @@ int main() {
     group2TrainingFile = fopen(group2_training_filePath, "r");
     group1TestFile = fopen(group1_test_filePath, "r");
     group2TestFile = fopen(group2_test_filePath, "r");
+    outputFile = fopen(outputFilePath, "w");
 
     // Initializing weights
     weights = (double *) calloc(dictionary.length, sizeof(double));
 
     // Initializing training datasets, weights and files
-    numberOfTrainingExamples = calculateLineNumber(group1_training_filePath)+calculateLineNumber(group2_training_filePath);
-    trainingHotVectors = (bool **) calloc(numberOfTrainingExamples, sizeof(bool*));
+    numberOfTrainingSamples = calculateLineNumber(group1_training_filePath)+calculateLineNumber(group2_training_filePath);
+    trainingHotVectors = (bool **) calloc(numberOfTrainingSamples, sizeof(bool*));
 
     // Çift sayılara grup 1, Tek sayılara grup 2
-    for (i = 0; i < numberOfTrainingExamples; i++) {
+    for (i = 0; i < numberOfTrainingSamples; i++) {
         trainingHotVectors[i] = (bool *) calloc(dictionary.length, sizeof(bool));
         if(i%2 == 0){
             makeHotVector(group1TrainingFile, dictionary, trainingHotVectors[i]);
@@ -81,11 +88,11 @@ int main() {
     }
 
     // Initializing test datasets
-    numberOfTestExamples = calculateLineNumber(group1_test_filePath)+calculateLineNumber(group2_test_filePath);
-    testHotVectors = (bool **) calloc(numberOfTrainingExamples, sizeof(bool*));
+    numberOfTestSamples = calculateLineNumber(group1_test_filePath)+calculateLineNumber(group2_test_filePath);
+    testHotVectors = (bool **) calloc(numberOfTestSamples, sizeof(bool*));
 
     // Çift sayılara grup 1, Tek sayılara grup 2
-    for (i = 0; i < numberOfTestExamples; i++) {
+    for (i = 0; i < numberOfTestSamples; i++) {
         testHotVectors[i] = (bool *) calloc(dictionary.length, sizeof(bool));
         if(i%2 == 0){
             makeHotVector(group1TestFile, dictionary, testHotVectors[i]);
@@ -102,26 +109,30 @@ int main() {
     double B1= 0.9;
     double B2= 0.99;
     int stoch_size = 10;
+    double duration;
     start_time = clock();
 
     switch (OPFUNC)
     {
-    case 0:
-        gradientDescent(epochs, numberOfTrainingExamples, step_size, dictionary.length, weights, trainingHotVectors);
-        break;
-    case 1:
-        stochasticGradientDescent(epochs, numberOfTrainingExamples, step_size, dictionary.length, weights, trainingHotVectors,stoch_size);
-        break;
-    case 2:
-        ADAM(epochs, numberOfTrainingExamples, step_size, dictionary.length, weights, trainingHotVectors,B1 ,B2,stoch_size);
-    default:
-        break;
+        case 0:
+            gradientDescent(epochs, numberOfTrainingSamples, step_size, dictionary.length, weights, trainingHotVectors, testHotVectors, numberOfTestSamples, outputFile);
+            break;
+        case 1:
+            stochasticGradientDescent(epochs, numberOfTrainingSamples, step_size, dictionary.length, weights, trainingHotVectors, stoch_size, testHotVectors, numberOfTestSamples, outputFile);
+            break;
+        case 2:
+            ADAM(epochs, numberOfTrainingSamples, step_size, dictionary.length, weights, trainingHotVectors,B1 ,B2,stoch_size, testHotVectors, numberOfTestSamples, outputFile);
+        default:
+            break;
     }
 
-    printf("duration: %.2f\n", (double)(clock()-start_time) / CLOCKS_PER_SEC * 1000);
+    // Print Overall Duration
+    printf("\n-------Training is Over-------\n");
+    duration = (double)(clock()-start_time) / CLOCKS_PER_SEC;
+    printf("duration: %.2f sec\n", duration);
 
     // Testing
-    test(testHotVectors, weights, dictionary.length, numberOfTestExamples);
+    test(testHotVectors, weights, dictionary.length, numberOfTestSamples);
 
     // Closing and Freeing
     fclose(group1TrainingFile);
@@ -138,15 +149,16 @@ void setValuesToArray(double* arr, double value, int length){
     }
 }
 
-double gradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors){
+void gradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors, bool** testHotVectors, int numberOfTestSamples, FILE* outputFile){
     int i,j,epoch, y;
-    double loss,gt;
+    double loss, gt, duration = 0, successRate;
     double *dp;
+    clock_t startTime;
+
     dp = malloc(exampleNumber*sizeof(double));
     for (epoch = 0; epoch < maxIter; epoch++) {
+        startTime = clock();
         loss = 0;
-        
-        
         for (i = 0; i < exampleNumber; i++) {
             dp[i] = dotProduct(weights, hotVectors[i], dictLength);
         }
@@ -170,22 +182,27 @@ double gradientDescent(int maxIter, int exampleNumber, double learning_rate, int
             loss += lossFunction(y, function(dp[i]));
         }
         loss /= exampleNumber;
-        printf("Epoch: %d, Loss: %f\n", epoch+1, loss);
+        duration += (double)(clock()-startTime) / CLOCKS_PER_SEC;
+        successRate = test(testHotVectors, weights, dictLength, numberOfTestSamples);
+
+        // Save statistics
+        //printf("Epoch: %d, Duration: %.2fs, Loss: %f, Succes Rate: %.2f\n", epoch+1, duration, loss, successRate);
+        fprintf(outputFile,"%d,%lf,%lf,%lf\n", epoch+1, duration, loss, successRate);
     }
-    return loss;
 }
 
-double stochasticGradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors,int stoch_size){
+void stochasticGradientDescent(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors,int stoch_size, bool** testHotVectors, int numberOfTestSamples, FILE* outputFile){
     int i,j,epoch,y;
-    double loss;
-    int start, end;
+    double loss, duration = 0, successRate;
+    clock_t startTime;
     double *dp;
     int * random_num;
     random_num = malloc(stoch_size*sizeof(int));
     dp = malloc(exampleNumber*sizeof(double));
     for (epoch = 0; epoch < maxIter; epoch++) {
         loss = 0;
-        
+        startTime = clock();
+
         for (i=0; i<stoch_size; i++){
             random_num[i] = rand() % exampleNumber;
             dp[i] = dotProduct(weights, hotVectors[random_num[i]], dictLength);
@@ -203,14 +220,19 @@ double stochasticGradientDescent(int maxIter, int exampleNumber, double learning
             loss += lossFunction(y, function(dp[i]));
         }
         loss /= exampleNumber;
-        printf("Epoch: %d, Loss: %f\n", epoch+1, loss);
+        duration += (double)(clock()-startTime) / CLOCKS_PER_SEC;
+        successRate = test(testHotVectors, weights, dictLength, numberOfTestSamples);
+
+        // Save statistics
+        //printf("Epoch: %d, Duration: %.2fs, Loss: %f, Succes Rate: %.2f\n", epoch+1, duration, loss, successRate);
+        fprintf(outputFile,"%d,%lf,%lf,%lf\n", epoch+1, duration, loss, successRate);
     }
-    return loss;
 }
 
-double ADAM(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors, double b1,double b2,int stoch_size){
+void ADAM(int maxIter, int exampleNumber, double learning_rate, int dictLength, double* weights, bool** hotVectors, double b1,double b2,int stoch_size, bool** testHotVectors, int numberOfTestSamples, FILE* outputFile){
     int i,j,epoch,y;
-    double loss;
+    double loss, duration = 0, successRate;
+    clock_t startTime;
     double * dp;
     double *mt,*vt,gt,eps=0.0000001;
     int  * random_num;
@@ -219,8 +241,8 @@ double ADAM(int maxIter, int exampleNumber, double learning_rate, int dictLength
     mt = calloc(dictLength,sizeof(double));
     vt= calloc(dictLength,sizeof(double));
     for (epoch = 0; epoch < maxIter; epoch++) {
-        
         loss = 0;
+        startTime = clock();
         for (i=0; i<stoch_size; i++){
             random_num[i] = rand() % exampleNumber;
             dp[i] = dotProduct(weights, hotVectors[random_num[i]], dictLength);
@@ -228,31 +250,35 @@ double ADAM(int maxIter, int exampleNumber, double learning_rate, int dictLength
         for (j = 0; j < dictLength; j++) {
             for (i = 0; i < stoch_size; i++) {
                 y= (random_num[i]%2) *(-2) +1 ;
-                
+
                 gt+=lossFunction_der(dp[i], y, hotVectors[random_num[i]][j]);
-                
+
             }
             gt /= stoch_size;
             // ADAM
             mt[j]=mt[j]*b1 + (1-b1) * gt;
             vt[j]= vt[j]*b2 + (1-b2) * gt * gt;
             weights[j] -= learning_rate * (mt[j]/(1-pow(b1,epoch+1))) / pow((vt[j]/(1-pow(b2,epoch+1)) )+ eps,0.5);
-            gt=0; 
+            gt=0;
         }
-       
+
         for (i=0; i<stoch_size; i++){
             y= (random_num[i]%2) *(-2) +1 ;
             dp[i] = dotProduct(weights, hotVectors[random_num[i]], dictLength);
             loss += lossFunction(y, function(dp[i]));
         }
         loss /= stoch_size;
-        printf("Epoch: %d, Loss: %f\n", epoch+1, loss);
+        duration += (double)(clock()-startTime) / CLOCKS_PER_SEC;
+        successRate = test(testHotVectors, weights, dictLength, numberOfTestSamples);
+
+        // Save statistics
+        //printf("Epoch: %d, Duration: %.2fs, Loss: %f, Succes Rate: %.2f\n", epoch+1, duration, loss, successRate);
+        fprintf(outputFile,"%d,%lf,%lf,%lf\n", epoch+1, duration, loss, successRate);
     }
     free(dp);
     free(random_num);
     free(mt);
     free(vt);
-    return loss;
 }
 
 double test(bool** hotVectors, double* weights, int dictLength, int exampleNumber){
@@ -268,12 +294,8 @@ double test(bool** hotVectors, double* weights, int dictLength, int exampleNumbe
         }
     }
     successRate = (double)success / exampleNumber;
-    printf("Your model has correctly predicted %d words out of %d\n", success, exampleNumber);
-    printf("Your model is %.2f percent successful!!\n", successRate*100);
+//    printf("Your model has correctly predicted %d words out of %d\n", success, exampleNumber);
+//    printf("Your model is %.2f percent successful!!\n", successRate*100);
     return successRate;
-}
-
-void saveStatistics(char* path, double loss, double succesRate, double duration){
-
 }
 
